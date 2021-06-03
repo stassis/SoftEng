28ad8ae578159bba88a358cb8c90483a8b061eff
oyosss <84403340+oyosss@users.noreply.github.com>
2021-06-03 12:11:45 +0000

    Add files via upload

---
+++ idioktitis_orario_ergasias/orario_info.java
@@ -0,0 +1,55 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class orario_info extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					orario_info frame = new orario_info();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public orario_info() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Epexergasia");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				dispose();
+				eisagogi_stoixeion es= new eisagogi_stoixeion();
+				es.setVisible(true);
+			}
+		});
+		btnNewButton.setBounds(153, 93, 106, 33);
+		contentPane.add(btnNewButton);
+	}
+
+}
