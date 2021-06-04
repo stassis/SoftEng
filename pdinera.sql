0e0a0657eed57b3b27165b495d8760b1e1ebc1bf
Kapnisis <83704126+Kapnisis@users.noreply.github.com>
2021-06-04 22:03:43 +0300

    Added user

--- pdinera.sql
+++ pdinera.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 02, 2021 at 12:34 AM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.3
+-- Generation Time: Jun 04, 2021 at 09:02 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,14 +39,54 @@ CREATE TABLE `food` (
 --
 
 INSERT INTO `food` (`Fagito`, `Posotita`, `Imerominia`, `Oriaellipsis`) VALUES
-('Zanzikin', 8, '2021-06-01', 6),
-('Kromydi', 9, '2021-06-01', 2),
-('Potaten', 29, '2021-06-01', 8),
-('Maroyli', 35, '2021-06-01', 2),
-('Ntomata', 40, '2021-06-01', 11),
 ('Chicken', 10, '2021-06-02', 2),
 ('Spaghetti', 22, '2021-06-02', 2),
 ('Pork', 7, '2021-06-02', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user2`
+--
+
+CREATE TABLE `user2` (
+  `id` int(11) NOT NULL,
+  `Role` tinytext NOT NULL,
+  `Password` tinytext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user2`
+--
+
+INSERT INTO `user2` (`id`, `Role`, `Password`) VALUES
+(1, 'Owner', 'ow1'),
+(11, 'Chef', 'ch1'),
+(12, 'Customer', 'hunter2'),
+(13, 'Chef', 'vvv'),
+(14, 'Customer', 'panos'),
+(15, 'Delivery', 'velos'),
+(16, 'Delivery', 'nomos.ilias');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user2`
+--
+ALTER TABLE `user2`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user2`
+--
+ALTER TABLE `user2`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
