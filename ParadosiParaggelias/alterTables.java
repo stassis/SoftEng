62a4e1eb4efa89cd16050ead7130c6b30f64013d
Stefanos Tassis <74511986+sttasg7@users.noreply.github.com>
2021-06-09 20:35:40 +0300

    remove-main-function

--- ParadosiParaggelias/alterTables.java
+++ ParadosiParaggelias/alterTables.java
@@ -21,22 +21,6 @@ public class alterTables extends JFrame {
 	private JPanel contentPane;
 
 	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					alterTables frame = new alterTables();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
 	 * Create the frame.
 	 */
 	public alterTables() {
