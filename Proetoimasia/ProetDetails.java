4070eb0e06a5e5ba9c8d1461404c7bc1b1d03eee
Kapnisis <83704126+Kapnisis@users.noreply.github.com>
2021-06-09 12:10:58 +0300

    delivery notified

--- Proetoimasia/ProetDetails.java
+++ Proetoimasia/ProetDetails.java
@@ -231,10 +231,13 @@ public class ProetDetails {
         			&&ready3.getText().equals("Ready")
         			&&ready4.getText().equals("Ready")) {
         			
-        			new ProetUpdate(tableModel,tableModel2,tableModel3,tableModel4);
-        			frame.dispose();
         			JOptionPane.showMessageDialog(null, "Delivery notified");
-        			//new Notification
+        			new Notification("delivery",1,order);
+        			
+        			ProetUpdate(tableModel,tableModel2,tableModel3,tableModel4);
+        			setOrderReady(order);
+        			frame.dispose();
+        			
         			new MainMenu();}
         		
         		else {JOptionPane.showMessageDialog(null, "Not Everything Ready");}
@@ -250,8 +253,74 @@ public class ProetDetails {
 		}
 
 
-//public void updatemodels(DefaultTableModel tm) {
+	
+public void ProetUpdate(DefaultTableModel tm1, DefaultTableModel tm2, DefaultTableModel tm3,
+				DefaultTableModel tm4) {
+		try {
+	    	Class.forName("com.mysql.cj.jdbc.Driver");
+	    	Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/pdinera","root","");
+	    	String sql = "SELECT COUNT(*) AS total FROM food";
+	    	Statement stmt = c.createStatement();
+	    	ResultSet rs = stmt.executeQuery(sql);
+	    				
+	    	rs.next();
+	    	int rows = rs.getInt("total");
+	    	//System.out.println(rows);
+	    						
+	    	for(int a=0; a<rows; a++) {
+	    		if((String)tm1.getValueAt(a, 2) == null) { }
+	    		else {
+	    			int tmp = Integer.parseInt((String)tm1.getValueAt(a,2));
+	    			String fagito = (String)tm1.getValueAt(a,0);
+	    			sql = "UPDATE `food` SET `Posotita` = `Posotita` - '"+tmp+"' WHERE `fagito` = '"+fagito+"'";
+	    			//System.out.println(tmp+"  "+fagito);
+	    			stmt.executeUpdate(sql);	}
+	    			}
+	    	
+	    	for(int a=0; a<rows; a++) {
+	    		if((String)tm2.getValueAt(a, 2) == null) { }
+	    		else {
+	    			int tmp = Integer.parseInt((String)tm2.getValueAt(a,2));
+	    			String fagito = (String)tm2.getValueAt(a,0);
+	    			sql = "UPDATE `food` SET `Posotita` = `Posotita` - '"+tmp+"' WHERE `fagito` = '"+fagito+"'";
+	    			//System.out.println(tmp+"  "+fagito);
+	    			stmt.executeUpdate(sql);	}
+	    			}
+	    	
+	    	for(int a=0; a<rows; a++) {
+	    		if((String)tm3.getValueAt(a, 2) == null) { }
+	    		else {
+	    			int tmp = Integer.parseInt((String)tm3.getValueAt(a,2));
+	    			String fagito = (String)tm3.getValueAt(a,0);
+	    			sql = "UPDATE `food` SET `Posotita` = `Posotita` - '"+tmp+"' WHERE `fagito` = '"+fagito+"'";
+	    			//System.out.println(tmp+"  "+fagito);
+	    			stmt.executeUpdate(sql);	}
+	    			}
+	    	
+	    	for(int a=0; a<rows; a++) {
+	    		if((String)tm4.getValueAt(a, 2) == null) { }
+	    		else {
+	    			int tmp = Integer.parseInt((String)tm4.getValueAt(a,2));
+	    			String fagito = (String)tm4.getValueAt(a,0);
+	    			sql = "UPDATE `food` SET `Posotita` = `Posotita` - '"+tmp+"' WHERE `fagito` = '"+fagito+"'";
+	    			//System.out.println(tmp+"  "+fagito);
+	    			stmt.executeUpdate(sql);	}
+	    			}
+	    	
+	    	
+	    		c.close();stmt.close();rs.close();
+	    				
+	    		}catch (Exception ee) {ee.printStackTrace();}
+		}
 	
 
-//} 
+public void setOrderReady(int order) {
+	try {
+    	Class.forName("com.mysql.cj.jdbc.Driver");
+    	Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/pdinera","root","");
+    	String sql = "UPDATE orders SET flag = 'ready' WHERE order_id = '"+order+"'";
+    	Statement stmt = c.createStatement();
+    	stmt.executeUpdate(sql);
+	}catch (Exception ee) {ee.printStackTrace();}
+	}
 }
