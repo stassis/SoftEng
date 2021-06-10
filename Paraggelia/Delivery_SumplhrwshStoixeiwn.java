5283797f3e2f6548275329485322809d20fde5b0
Andreasf02 <83708129+Andreasf02@users.noreply.github.com>
2021-06-10 19:15:26 +0300

    Add files via upload

--- Paraggelia/Delivery_SumplhrwshStoixeiwn.java
+++ Paraggelia/Delivery_SumplhrwshStoixeiwn.java
@@ -115,16 +115,16 @@ public class Delivery_SumplhrwshStoixeiwn extends JFrame {
 		        			
         				}else {
             				
-            				JOptionPane.showMessageDialog(null, "Eisagete thetikes times(akeraies)!");
+            				JOptionPane.showMessageDialog(null, "Enter positive values(integer)!");
             				
             			}
         			}catch(NumberFormatException e1) {
-        				JOptionPane.showMessageDialog(null, "Eisagete thetikes times(akeraies)!"); 
+        				JOptionPane.showMessageDialog(null, "Enter positive values(integer)!"); 
      				   
      				 } 	
         		}else {
         			
-        			JOptionPane.showMessageDialog(null, "Symplhrwste stoixeia!");
+        			JOptionPane.showMessageDialog(null, "Please fill in the textfields!");
         			
         		}
         		
